6dc7f958f5b20e39073f52ecdcf2a88801cfcc0b
syky <sergiy.kucheryavyy@gft.com>
2021-11-10 13:37:18 +0100

    Test commit2

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/java/resources/features"},
         plugin = {"json:target/jsonReports/cucumber-report.json"},
         glue = {"steps"},
-        tags = "@base")
+        tags = "@carousel")
 
 public class TestRunner {
 }
